cb03fb5074dd252da2f305565edcd863d8eef5cf
taka.baya <kamiyabi12@gmail.com>
2018-10-31 18:50:04 +0900

    first commit of getfaceData

---
+++ getFaceData/Extraction_face.py
@@ -0,0 +1,41 @@
+ # -*- coding:utf-8 -*-
+import numpy
+import cv2
+cascade_path = 'C:/Users/kambayt/Documents/anythings/python/recognize_face/xml/haarcascade_frontalface_alt.xml'
+
+def ReadImage():
+    for i in range(1, 7):
+        filename = "%02d.jpg" % i
+        
+        #ファイル読み込み
+        image = cv2.imread(filename)
+        #グレースケール変換
+        image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        #カスケード分類器の特徴量を取得する
+        cascade = cv2.CascadeClassifier(cascade_path)
+        #物体認識（顔認識）の実行
+        facerect = cascade.detectMultiScale(
+                        image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+        
+        print(filename) 
+        print(len(facerect))
+        
+        if len(facerect) <= 0:
+            continue
+        
+        rect = facerect[0]
+        for r in facerect:
+            if rect[2] < r[2]:
+                rect = r
+            
+        
+        x = rect[0]
+        y = rect[1]
+        w = rect[2]
+        h = rect[3]
+        
+        # img[y: y + h, x: x + w] 
+        cv2.imwrite('face_' + filename, image_gray[y:y+h, x:x+w])
+
+if __name__ == '__main__':
+    ReadImage()
